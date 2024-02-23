dababd6dc62e1abd4c13d1e5d32dbf83d090cbb6
Mai Trux <maitrux@hotmail.com>
2024-02-23 17:40:39 +0100

    added documentation for building to prod

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+# HOW TO DEPLOY
+# First build the project with `npm run build`
+# Then run the commands of this script one by one
+
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:maitrux/yatzy.git main:gh-pages
+
+cd -
