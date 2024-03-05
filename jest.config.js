e28cf27632059daa789b8b9f2a78c34466131130
Mai Trux <maitrux@hotmail.com>
2024-03-05 20:12:44 +0100

    added coverage report when runnin tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   testEnvironment: "jest-environment-jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/ScoreSheet.ts"],
+  coverageDirectory: "coverage",
 };
